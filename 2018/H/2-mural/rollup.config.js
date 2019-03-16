c4829e8e1752cf69c6c4da81b63656980515104f
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-03-17 01:11:25 +0800

    restructure files.

--- 2018/H/2-mural/rollup.config.js
+++ 2018/H/2-mural/rollup.config.js
@@ -2,9 +2,9 @@ import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 
 export default {
-  input: 'solution.js',
+  input: 'src/solution.js',
   output: {
-    file: 'solution.build.js',
+    file: 'build/solution.js',
     format: 'cjs'
   },
   plugins: [
